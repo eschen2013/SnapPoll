5d3bdc15137af43f071282fba90436df347930ca
Eric Chen <eschen@gmail.com>
2014-01-04 23:22:43 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SnapPoll::Application.config.secret_key_base = 'eb8a8df051e242e0f5720bee741e2f01d4287c539f5501c20be32990fc716c385fa102be5fb4a112baeab4ee28f4e1c77392ff9e015480de6a74ad77cf5eac0b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SnapPoll::Application.config.session_store :cookie_store, key: '_SnapPoll_session'
