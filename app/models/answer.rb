8c3568f3b21224433e3a40cc64e4f2f14db67720
Eric Chen <eschen@gmail.com>
2014-01-10 02:21:16 -0500

    Get rid of blank votes.

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,4 +2,9 @@ class Answer < ActiveRecord::Base
   belongs_to :poll
   has_many :votes
   mount_uploader :image, ImageUploader
+
+  def blank?
+    (!body || body.empty?) && (image.blank?)
+  end
+
 end
